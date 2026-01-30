4846f9083a0afebe5f43ea2843bfa0023b1e433b
ǝɯɹǝɥlınɓ <69699602+guilhermedallagnoli@users.noreply.github.com>
2026-01-30 22:04:51 +0000

    Etapa 3: Implementação do código fonte para passar nos testes

--- src/IMCService.java
+++ src/IMCService.java
@@ -1,8 +1,18 @@
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class IMCService {
     public double calcularIMC(Double peso, Double altura) {
         if (peso == null) throw new IllegalArgumentException("peso obrigatório");
         if (altura == null || altura == 0) throw new IllegalArgumentException("altura inválida");
-        return Math.round((peso / (altura * altura)) * 100.0) / 100.0; // arredonda 2 casas
+        
+        BigDecimal pesoBD = new BigDecimal(peso);
+        BigDecimal alturaBD = new BigDecimal(altura);
+        
+        BigDecimal imc = pesoBD.divide(alturaBD.multiply(alturaBD), 10, RoundingMode.HALF_UP);
+        imc = imc.setScale(2, RoundingMode.HALF_UP);
+        
+        return imc.doubleValue();
     }
 }
